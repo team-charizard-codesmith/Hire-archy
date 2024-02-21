add20106b28fa44975134c67ee15d2d370211324
Oliver-Cho <ocho@wesleyan.edu>
2024-02-21 14:57:50 -0500

    worked on creating jobController

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -1,20 +1,64 @@
-const { query } = require('express');
-const userModel = require('../models/userModel');
+const { query } = require("express");
+const db = require("../models/database");
 
-const userController = {};
+const jobController = {};
 
-userController.getUsers = (req, res, next) => {
-    const query = 'SELECT * FROM users';
-    try {
-        userModel.query(query)
-            .then((data) => {
-                console.log(data);
-                return next();
-            })
-    }
-    catch (err) {
-        next(err);
-    }
-}
+jobController.addJob = (req, res, next) => {
+	const { title, salary, open_date, address, company_id, user_id } = req.body;
+	const newJobQuery =
+		"INSERT INTO jobs" +
+		`(title, salary, open_date, address, company_id, user_id)` +
+		`VALUES($1, $2, $3, $4, $5, $6)`;
+	const arr = [title, salary, open_date, address, company_id, user_id];
+	try {
+		db.query(newJobQuery, arr).then((data) => {
+			console.log(data);
+			console.log(`Successfully inserted ${arr} into jobs`);
+			return next();
+		});
+	} catch (err) {
+		next(err);
+	}
+};
+jobController.getJobs = (req, res, next) => {
+	const getJobsquery = "SELECT * FROM jobs";
+	try {
+		db.query(getJobsquery).then((data) => {
+			console.log(data);
+			res.locals.jobs = data;
+			return next();
+		});
+	} catch (err) {
+		next(err);
+	}
+};
 
-module.exports = userController;
+jobController.updateJob = (req, res, next) => {
+	const { id, title, salary, open_date, address, company_id, user_id } =
+		req.body;
+	const updateJobQuery = "UPDATE jobs" + `SET title=`;
+	try {
+		db.query(updateJobQuery).then((data) => {
+			console.log(data);
+			console.log(`Successfully updated ${arr} into jobs`);
+			return next();
+		});
+	} catch (err) {
+		next(err);
+	}
+};
+jobController.deleteJob = (req, res, next) => {
+	const { id } = req.body;
+	const deleteJobQuery = "DELETE FROM jobs" + `WHERE id=${id}`;
+	try {
+		db.query(deleteJobQuery).then((data) => {
+			console.log(data);
+			console.log(`Successfully deleted job #${id} from jobs`);
+			return next();
+		});
+	} catch (err) {
+		next(err);
+	}
+};
+
+module.exports = jobController;
